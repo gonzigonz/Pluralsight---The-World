272bf1d1af5e76943df790d6bfa954c3fcfdc460
Gonzalo Lucero <gonzalo.lucero@jjhonline.com>
2015-12-11 14:31:15 +1100

    JavaScript - Events (Vanilla)

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,4 +1,22 @@
 // site.js
 
-var ele = document.getElementById("username");
+var ele = document.getElementById('username');
 ele.innerHTML = 'Freddy Junior';
+
+var main = document.getElementById('main');
+main.onmouseenter = function () {
+	// Firefox version
+	//main.style = 'background-color: #888;';
+
+	// IE & Google Chrome version
+	main.style['background-color'] = '#888';
+	
+};
+main.onmouseleave = function () {
+	// Firefox Version
+	//main.style = '';
+
+	// IE & Google Chrome version
+	main.style['background-color'] = '';
+	
+};
