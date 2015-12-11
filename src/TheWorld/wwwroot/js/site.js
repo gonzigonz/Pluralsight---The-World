aab21dc3614554dff8e5936a2df3a6358a4250ad
Gonzalo Lucero <gonzalo.lucero@jjhonline.com>
2015-12-11 16:16:47 +1100

    JavaScript - Global Scope
    - Added Immediately-Invoked Function Expression (iife)

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,22 +1,26 @@
 // site.js
 
-var ele = document.getElementById('username');
-ele.innerHTML = 'Freddy Junior';
+(function () {
 
-var main = document.getElementById('main');
-main.onmouseenter = function () {
-	// Firefox version
-	//main.style = 'background-color: #888;';
+	var ele = document.getElementById('username');
+	ele.innerHTML = 'Freddy Junior';
 
-	// IE & Google Chrome version
-	main.style['background-color'] = '#888';
-	
-};
-main.onmouseleave = function () {
-	// Firefox Version
-	//main.style = '';
+	var main = document.getElementById('main');
+	main.onmouseenter = function () {
+		// Firefox version
+		//main.style = 'background-color: #888;';
 
-	// IE & Google Chrome version
-	main.style['background-color'] = '';
-	
-};
+		// IE & Google Chrome version
+		main.style['background-color'] = '#888';
+
+	};
+	main.onmouseleave = function () {
+		// Firefox Version
+		//main.style = '';
+
+		// IE & Google Chrome version
+		main.style['background-color'] = '';
+
+	};
+
+})();
