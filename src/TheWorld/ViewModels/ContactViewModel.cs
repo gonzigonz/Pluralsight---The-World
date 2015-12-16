3408eb93cf89f8339bad7dc550e6aacfe5f7cbfd
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-16 12:02:52 +1100

    MVC 6 - Using Validation
    Added MVC validation data annotations to our view mode.
    Used bower to include ‘jquery-validation’ and
    ‘jquery-validation-unobtrusive’ for ASP.NET client side validation.

--- src/TheWorld/ViewModels/ContactViewModel.cs
+++ src/TheWorld/ViewModels/ContactViewModel.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TheWorld.ViewModels
 {
 	public class ContactViewModel
 	{
+		[Required]
+		[StringLength(255, MinimumLength = 5)]
 		public string Name { get; set; }
+		
+		[Required]
+		[EmailAddress]
 		public string Email { get; set; }
+		
+		[Required]
+		[StringLength(1024, MinimumLength = 5)]
 		public string Message { get; set; }
 	}
 }
