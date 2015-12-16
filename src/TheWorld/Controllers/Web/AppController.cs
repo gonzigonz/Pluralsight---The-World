a49fbceb5d96b75826d990e2ab51455624bf6550
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-16 14:21:49 +1100

    MVC 6 - Supporting POST

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNet.Mvc;
-using System;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
@@ -16,6 +16,12 @@ namespace TheWorld.Controllers.Web
 		}
 		
 		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Contact(ContactViewModel model)
 		{
 			return View();
 		}
