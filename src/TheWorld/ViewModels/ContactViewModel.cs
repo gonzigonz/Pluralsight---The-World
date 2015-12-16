30625c193c360ef39286d0d06a3ef6f6634c0d8a
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-16 11:34:19 +1100

    MVC 6 - Implementing Contact Page
    Added ‘Contact’ view mode.
    Added contact form and used ‘asp-for’ tag helpers to automatically add
    html for, name and id tags to the form.

---
+++ src/TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheWorld.ViewModels
+{
+	public class ContactViewModel
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Message { get; set; }
+	}
+}
