cd4f151243784d1a74adc94f84198ffd64fda917
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-18 16:14:53 +1100

    Creating the API - Create an API Controller

---
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripController : Controller
+    {
+        [HttpGet("api/trips")]
+        public JsonResult Get()
+        {
+			return Json(new { name = "Gonz" });
+        }
+    }
+}
