a5a113780a47e3a270fc5fe52f8c6af01345898d
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-23 16:54:40 +1100

    ASP.NET 5 Identity - Designing the Login View

---
+++ src/TheWorld/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TheWorld.Controllers
+{
+	public class AuthController: Controller
+    {
+		public IActionResult Login()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				return RedirectToAction("Trips", "Apps");
+			}
+
+			return View();
+		}
+    }
+}
