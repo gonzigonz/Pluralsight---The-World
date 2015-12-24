2732f143497a70b80d75d495ed5826982c5a5094
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-24 17:36:22 +1100

    ASP.NET 5 Deployment - Hosting Environment

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -90,9 +90,23 @@ namespace TheWorld
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public async void Configure(IApplicationBuilder app, WorldContextSeedData seedData, ILoggerFactory loggerFactory)
+		public async void Configure(
+			IApplicationBuilder app, 
+			WorldContextSeedData seedData, 
+			ILoggerFactory loggerFactory,
+			IHostingEnvironment env)
         {
-			loggerFactory.AddDebug(LogLevel.Warning);
+			if (env.IsDevelopment())
+			{
+				loggerFactory.AddDebug(LogLevel.Information);
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				loggerFactory.AddDebug(LogLevel.Error);
+				app.UseExceptionHandler("/App/Error");
+			}
+			
 
 			app.UseStaticFiles();
 
