e5d3ca53a2fef682c68ef6f8c6c1f90573533a2e
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-24 12:49:29 +1100

    AngularJS Routing - Calling the API

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -4,13 +4,25 @@
 
 	angular.module('app-trips').controller('tripEditorController', tripEditorController);
 
-	function tripEditorController($routeParams) {
+	function tripEditorController($routeParams, $http) {
 		var vm = this;
 
 		vm.tripName = $routeParams.tripName;
 		vm.stops = [];
 		vm.errorMessage = "";
 		vm.isBusy = true;
+
+		$http.get("/api/trips/" + vm.tripName + "/stops")
+			.then(function (res) {
+				// Success
+				angular.copy(res.data, vm.stops)
+			}, function (err) {
+				// Failure
+				vm.errorMessage = "Failed to load stops: " + err;
+			})
+			.finally(function () {
+				vm.isBusy = false
+			});
 	};
 
 })();
