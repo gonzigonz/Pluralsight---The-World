0db737555ea55238796acdf44536eb7a520fa947
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-24 17:06:47 +1100

    ASP.NET 5 Deployment - Minifying your Javascript
    Introduced nom and gulp to the project and configured a gulp task to
    run after the build process.

---
+++ src/TheWorld/gulpfile.js
@@ -0,0 +1,14 @@
+﻿/// <binding AfterBuild='minify' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+gulp.task('minify', function () {
+	return gulp.src('wwwroot/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('wwwroot/lib/_app'));
+});
