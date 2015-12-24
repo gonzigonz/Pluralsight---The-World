065b49d04eced1478b7df809f5b6139eab0f51c3
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-24 17:28:43 +1100

    ASP.NET 5 Deployment - Minifying AngularJS Code

--- src/TheWorld/gulpfile.js
+++ src/TheWorld/gulpfile.js
@@ -6,9 +6,11 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('minify', function () {
 	return gulp.src('wwwroot/js/*.js')
+		.pipe(ngAnnotate())
 		.pipe(uglify())
 		.pipe(gulp.dest('wwwroot/lib/_app'));
 });
